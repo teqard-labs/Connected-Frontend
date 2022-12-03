84355fba200cbc48555692aaa034e306067a1d0e
josephgeorgepadayatty <josephgeorgepadayatty@gmail.com>
2022-12-03 23:14:31 +0530

    added navbar and hero page

---
+++ src/components/hero.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class Hero extends Component {
+    constructor(props) {
+        super(props);
+    }
+   
+    render() { 
+        return (<div>
+         <img src="https://mdbootstrap.com/img/new/standard/city/047.jpg" className="my-6 mx-16 max-w-full h-auto rounded-lg" alt="">
+            
+         </img>
+        </div>  );
+    }
+}
+ 
+export default Hero;
