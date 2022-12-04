65d733605719ca9aff6dbfbf12e3cd96f0541e6b
josephgeorgepadayatty <josephgeorgepadayatty@gmail.com>
2022-12-04 21:28:50 +0530

    added background images

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,15 +1,18 @@
 import React, { Component } from 'react'
-
+import img from '../media/Artboard 1.jpg'
+import Navbar from '../components/navbar';
 class Hero extends Component {
     constructor(props) {
         super(props);
     }
    
     render() { 
-        return (<div>
-         <img src="https://mdbootstrap.com/img/new/standard/city/047.jpg" className="my-6 mx-16 max-w-full h-auto rounded-lg" alt="">
+        return (<div style={{backgroundImage: 'url(${img})'}}>
+            <Navbar/>
+            <img src="https://mdbootstrap.com/img/new/standard/city/047.jpg" className="my-6 mx-16 max-w-full h-auto rounded-lg" alt=""></img>
             
-         </img>
+            
+         
         </div>  );
     }
 }
