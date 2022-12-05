ba4e5d1fe5754a507df8d695b819534a277ccd95
josephgeorgepadayatty <josephgeorgepadayatty@gmail.com>
2022-12-05 20:42:33 +0530

    added latest news interface and bgs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import Navbar from './components/navbar';
+
 import Hero from './components/hero';
 import './App.css';
 
 function App() {
   return (
-    <div className="w-screen  h-screen  bg-[#01004C] ">
-      <Navbar/>
+    <div>
+      
       <Hero/>
     </div>
   );
