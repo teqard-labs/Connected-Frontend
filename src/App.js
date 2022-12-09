550e6c1a9e0fc467b1768a98b6052cec5365cb23
SS_11_RED <55880200+SanathSavio@users.noreply.github.com>
2022-12-09 22:42:17 +0530

    added faq

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import img2 from './media/Artboard2.jpg'
 import img3 from './media/Artboard3.jpg'
 import img4 from './media/Artboard4.jpg'
 import Hero from './components/hero';
+import faq from './components/faq';
 import './App.css';
 
 function App() {
@@ -12,7 +13,7 @@ function App() {
       <Hero/>
       </div>
       <div className='w-full h-full dark:bg-[#01004C]'  style={{ backgroundImage:`url(${img2})`,backgroundRepeat:"no-repeat" }}>
-      
+      <Faq/>
       </div>
       <div className='w-full h-full dark:bg-[#01004C]'  style={{ backgroundImage:`url(${img3})`,backgroundRepeat:"no-repeat" }}>
       
